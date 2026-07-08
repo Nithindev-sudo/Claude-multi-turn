7a1ca23868bc2afc845470f818cafc471d4dcc7a
Nithin <dev.nithin.in@gmail.com>
2026-07-08 08:17:48 +0530

    agent call with max tokens 1000

---
+++ agent.py
@@ -0,0 +1,22 @@
+from dotenv import load_dotenv
+load_dotenv()
+from anthropic import Anthropic
+
+client = Anthropic()
+model="claude-sonnet-4-6"
+print(load_dotenv())
+message = client.messages.create(
+    
+        model=model,
+        max_tokens=1000,
+        messages = [
+            {
+                "role" : "user",
+                "content" : "Hi can you tell me what is meant by an MCP or controller in claude terms? give answer in one sentence"
+            }
+        ]
+    
+)
+
+print(message.content[0].text)
+
